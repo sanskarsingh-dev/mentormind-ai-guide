f69cb5811eeb80aa1b117c669ec5a923eab54f20
sanskarsingh-dev <sanskarsingh7007@gmail.com>
2025-11-25 16:44:45 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "next-themes";
 import App from "./App.tsx";
 import "./index.css";
+import "https://cdn.jsdelivr.net/npm/katex@0.16.10/dist/katex.min.css";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
